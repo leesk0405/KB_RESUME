902387dae097223574a69600f3a886cf92e8c418
leesk0405 <155949499+leesk0405@users.noreply.github.com>
2024-03-06 15:22:48 +0900

    12th commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 $('.top').click(function(){
     $('html,body').animate({
         scrollTop:0
-    },800)
+    },800);
 });
 
+$('.h_txt').click(function(){
+    // 현재 스크롤 위치를 세션 스토리지에 저장
+    sessionStorage.setItem('scrollPosition', $(window).scrollTop());
+    console.log(sessionStorage.getItem('scrollPosition'));
+
+    // 이전 페이지로 리디렉션
+    window.history.back();
+});
 
 // $('.section3').find('.row').attr("data-aos","fade-up");
 
